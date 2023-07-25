4d5dedd0b2e2b475741f26b003473caf87383382
Luca Pegolotti <pegolotti.luca@gmail.com>
2023-07-25 09:54:36 -0700

    Adapt train script to bloodflow_1d_mgn

--- examples/cfd/bloodflow_1d_mgn/constants.py
+++ examples/cfd/bloodflow_1d_mgn/constants.py
@@ -1,16 +1,22 @@
-# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2023 Stanford University
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
 
 import os
 from pathlib import Path
@@ -19,15 +25,10 @@ from typing import Tuple, Optional
 
 
 class Constants(BaseModel):
-    # data configs
-    data_dir: str = "./raw_dataset/cylinder_flow/cylinder_flow"
-
     # training configs
     batch_size: int = 100
     epochs: int = 25
-    num_training_samples: int = 400
-    num_training_time_steps: int = 300
-    lr: float = 0.0001
+    lr: float = 0.001
     lr_decay_rate: float = 0.9999991
 
     ckpt_path: str = "checkpoints"
--- examples/cfd/bloodflow_1d_mgn/train.py
+++ examples/cfd/bloodflow_1d_mgn/train.py
@@ -1,16 +1,22 @@
-# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2023 Stanford University
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
 
 import torch
 from dgl.dataloading import GraphDataLoader
@@ -44,68 +50,11 @@ import argparse
 # Instantiate constants
 C = Constants()
 
-def parse_command_line_arguments():
-    """
-    Parse command line arguments.
-
-    Returns:
-        Data structure containing all the arguments
-    """
-
-    # parse arguments from command line
-    parser = argparse.ArgumentParser(description='Graph Reduced Order Models')
-
-    parser.add_argument('--bs', help='batch size', type=int, default=100)
-    parser.add_argument('--epochs', help='total number of epochs', type=int,
-                        default=100)
-    parser.add_argument('--lr_decay', help='learning rate decay', type=float,
-                        default=0.001)
-    parser.add_argument('--lr', help='learning rate', type=float, default=0.001)
-    parser.add_argument('--rate_noise', help='rate noise', type=float,
-                        default=100)
-    parser.add_argument('--rate_noise_features', help='rate noise features', 
-                        type=float, default=1e-5)
-    parser.add_argument('--weight_decay', help='l2 regularization', 
-                        type=float, default=1e-5)
-    parser.add_argument('--ls_gnn', help='latent size gnn', type=int,
-                        default=16)
-    parser.add_argument('--ls_mlp', help='latent size mlps', type=int,
-                        default=64)
-    parser.add_argument('--process_iterations', help='gnn layers', type=int,
-                        default=3)
-    parser.add_argument('--hl_mlp', help='hidden layers mlps', type=int,
-                        default=2)
-    parser.add_argument('--label_norm', help='0: min_max, 1: normal, 2: none',
-                        type=int, default=1)
-    parser.add_argument('--stride', help='stride for multistep training',
-                        type=int, default=5)
-    parser.add_argument('--bcs_gnn', help='path to graph for bcs',
-                        type=str, default='models_bcs/31.10.2022_01.35.31')
-    args = parser.parse_args()
-
-    # we create a dictionary with all the parameters
-    t_params = {'latent_size_gnn': args.ls_gnn,
-                'latent_size_mlp': args.ls_mlp,
-                'process_iterations': args.process_iterations,
-                'number_hidden_layers_mlp': args.hl_mlp,
-                'learning_rate': args.lr,
-                'batch_size': args.bs,
-                'lr_decay': args.lr_decay,
-                'nepochs': args.epochs,
-                'weight_decay': args.weight_decay,
-                'rate_noise': args.rate_noise,
-                'rate_noise_features': args.rate_noise_features,
-                'stride': args.stride,
-                'bcs_gnn': args.bcs_gnn}
-
-    return t_params, args
-
-
 class MGNTrainer:
-    def __init__(self, wb, dist, rank_zero_logger):
-        self.dist = dist
+    def __init__(self, wb):
+        self.device="cuda:0"
 
-        t_params, args = parse_command_line_arguments()
+        t_params = {}
         norm_type = {'features': 'normal', 'labels': 'normal'}
         graphs, params = generate_normalized_graphs('raw_dataset/graphs/',
                                                     norm_type)
@@ -146,9 +95,6 @@ class MGNTrainer:
         train_graphs = [graphs[gname] for gname in trainset]
         traindataset = Bloodflow1DDataset(train_graphs, params, trainset)
 
-        # test_graphs = [graphs[gname] for gname in testset]
-        # traindataset = Bloodflow1DDataset(test_graphs, params, testset)
-
         # instantiate dataloader
         self.dataloader = GraphDataLoader(
             traindataset,
@@ -156,29 +102,25 @@ class MGNTrainer:
             shuffle=True,
             drop_last=True,
             pin_memory=True,
-            use_ddp=dist.world_size > 1,
+            use_ddp=False,
         )
-
         # instantiate the model
         self.model = MeshGraphNet(
-            17, params['infeat_edges'], 2
+            17, 
+            params['infeat_edges'], 
+            2,
+            processor_size=5,
+            hidden_dim_node_encoder=64,
+            hidden_dim_edge_encoder=64,
+            hidden_dim_node_decoder=64   
         )
-        if C.jit:
-            self.model = torch.jit.script(self.model).to(dist.device)
-        else:
-            self.model = self.model.to(dist.device)
-        if C.watch_model and not C.jit and dist.rank == 0:
-            wb.watch(self.model)
 
-        # distributed data parallel for multi-node training
-        if dist.world_size > 1:
-            self.model = DistributedDataParallel(
-                self.model,
-                device_ids=[dist.local_rank],
-                output_device=dist.device,
-                broadcast_buffers=dist.broadcast_buffers,
-                find_unused_parameters=dist.find_unused_parameters,
-            )
+        if C.jit:
+            self.model = torch.jit.script(self.model).to(self.device)
+        else:
+            self.model = self.model.to(self.device)
+        if C.watch_model and not C.jit:
+            wb.watch(self.model)
 
         # enable train mode
         self.model.train()
@@ -196,19 +138,17 @@ class MGNTrainer:
         self.scaler = GradScaler()
 
         # load checkpoint
-        if dist.world_size > 1:
-            torch.distributed.barrier()
         self.epoch_init = load_checkpoint(
             os.path.join(C.ckpt_path, C.ckpt_name),
             models=self.model,
             optimizer=self.optimizer,
             scheduler=self.scheduler,
             scaler=self.scaler,
-            device=dist.device,
+            device=self.device,
         )
 
     def train(self, graph):
-        graph = graph.to(self.dist.device)
+        graph = graph.to(self.device)
         self.optimizer.zero_grad()
         loss = self.forward(graph)
         self.backward(loss)
@@ -218,8 +158,8 @@ class MGNTrainer:
     def forward(self, graph):
         # forward pass
         with autocast(enabled=C.amp):
-            pred = self.model(graph.ndata["nfeatures"], 
-                              graph.edata["efeatures"], graph)
+            pred = self.model(graph, node_features = graph.ndata["nfeatures"], 
+                              edge_features = graph.edata["efeatures"])
             loss = self.criterion(pred, graph.ndata["delta"])
             return loss
 
@@ -235,57 +175,26 @@ class MGNTrainer:
 
 
 if __name__ == "__main__":
-    # initialize distributed manager
-    DistributedManager.initialize()
-    dist = DistributedManager()
-
-    # save constants to JSON file
-    # if dist.rank == 0:
-    #     os.makedirs(C.ckpt_path, exist_ok=True)
-    #     with open(
-    #         os.path.join(C.ckpt_path, C.ckpt_name.replace(".pt", ".json")), "w"
-    #     ) as json_file:
-    #         json_file.write(C.json(indent=4))
-
-    # initialize loggers
-    # initialize_wandb(
-    #     project="Modulus-Launch",
-    #     entity="Modulus",
-    #     name="Vortex_Shedding-Training",
-    #     group="Vortex_Shedding-DDP-Group",
-    #     mode=C.wandb_mode,
-    # )  # Wandb logger
-    logger = PythonLogger("main")  # General python logger
-    rank_zero_logger = RankZeroLoggingWrapper(logger, dist)  # Rank 0 logger
-    logger.file_logging()
-
-    trainer = MGNTrainer(wb, dist, rank_zero_logger)
+    trainer = MGNTrainer(wb)
     start = time.time()
-    rank_zero_logger.info("Training started...")
+    print("Training started...")
     for epoch in range(trainer.epoch_init, C.epochs):
         count = 0
         for graph in trainer.dataloader:
-            print('{:}/{:}'.format(count, len(trainer.dataloader)))
             loss = trainer.train(graph)
             count = count+1
-        print(f"epoch: {epoch}, loss: {loss:10.3e}, time per epoch: {(time.time()-start):10.3e}")
-        rank_zero_logger.info(
+        print(
             f"epoch: {epoch}, loss: {loss:10.3e}, time per epoch: {(time.time()-start):10.3e}"
         )
-        wb.log({"loss": loss.detach().cpu()})
 
         # save checkpoint
-        if dist.world_size > 1:
-            torch.distributed.barrier()
-        if dist.rank == 0:
-            save_checkpoint(
+        save_checkpoint(
                 os.path.join(C.ckpt_path, C.ckpt_name),
                 models=trainer.model,
                 optimizer=trainer.optimizer,
                 scheduler=trainer.scheduler,
                 scaler=trainer.scaler,
                 epoch=epoch,
-            )
-            logger.info(f"Saved model on rank {dist.rank}")
+        )
         start = time.time()
-    rank_zero_logger.info("Training completed!")
+    print("Training completed!")
